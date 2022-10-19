9b75c000f0fc61731e40a4eea7e4746e87200db5
Drew Layton <dlayton66@gmail.com>
2022-10-18 18:48:31 -0600

    feat: Add board attribute to Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,15 @@
 class Board
+  attr_reader :board
+  
   def initialize
+    @board = {
+      row_0: "ABCDEFG",
+      row_1: ".......",
+      row_2: ".......",
+      row_3: ".......",
+      row_4: ".......",
+      row_5: ".......",
+      row_6: ".......",
+      }
   end
 end
