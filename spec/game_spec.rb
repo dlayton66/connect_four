3ebb1931b303d239b5b1f41e9ec23ac13b7a28a4
Drew Layton <dlayton66@gmail.com>
2022-10-18 19:20:07 -0600

    test: Create #start board creation test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,4 +12,14 @@ describe Game do
       expect(@new_game).to be_a Game
     end
   end
+
+  describe '#start' do
+    before(:each) do
+      @new_game = Game.new
+    end
+
+    it 'creates a board' do
+      expect(@new_game.board).to be_a Board
+    end
+  end
 end
