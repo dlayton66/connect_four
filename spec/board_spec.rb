03b2a086f9ee2d38f3f3821945337c1e1cb676f1
Drew Layton <dlayton66@gmail.com>
2022-10-18 18:45:53 -0600

    test: Write stores the board test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,9 +3,24 @@ require './lib/board'
 
 describe Board do
 	describe '#initialize' do
+		before(:each) do
+			@board = Board.new
+		end
+
 		it 'is an instance of board' do
-			board = Board.new
-			expect(board).to be_a Board
+			expect(@board).to be_a Board
+		end
+
+		it 'stores the board' do
+			expect(@board.board).to eq({
+				row_0: "ABCDEFG",
+				row_1: ".......",
+				row_2: ".......",
+				row_3: ".......",
+				row_4: ".......",
+				row_5: ".......",
+				row_6: ".......",
+				})
 		end
 	end
 end
