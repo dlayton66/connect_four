f6470c953982c1a93d624c679e00dc5ab5bab115
Drew Layton <dlayton66@gmail.com>
2022-10-19 21:43:47 -0600

    refactor: Move user input checks to col_select

--- lib/game.rb
+++ lib/game.rb
@@ -19,10 +19,13 @@ class Game
   def col_select
     loop do
       input = gets.chomp
-      if ["a", "b", "c", "d", "e", "f", "g"].include? input.downcase 
-        return input
-      else
+      col = convert_input(input)
+      if !["a", "b", "c", "d", "e", "f", "g"].include? input.downcase 
         puts "Invalid input. Please enter a letter from A - G"
+      elsif !@new_board.playable_col.include? col
+        puts "That column is full.  Please select another."
+      else  
+        return col
       end
     end
   end
@@ -47,34 +50,27 @@ class Game
 
   def take_turn
     # Player turn
-    if @new_board.playable_col == []
-      puts "Tie game!"
-    else
-      col = convert_input(col_select)
-      row = @new_board.open_row[col]
-      if @new_board.playable_col.include? col
-        @new_board.board["row_#{row}".to_sym][col] = "X"
-        @new_board.open_row[col] -= 1
-        if @new_board.open_row[col] == 0
-          @new_board.playable_col.delete(col)
-        end
-      else
-        puts "That column is full.  Please select another."
-      end
+    col = col_select
+    row = @new_board.open_row[col]
+    
+    @new_board.board["row_#{row}".to_sym][col] = "X"
+    @new_board.open_row[col] -= 1
+    if @new_board.open_row[col] == 0
+      @new_board.playable_col.delete(col)
     end
 
     # Computer turn
-    if @new_board.playable_col == []
-      puts "Tie game!"
-    else
-      comp_col = @new_board.playable_col.sample
-      comp_row = @new_board.open_row[comp_col]
-      @new_board.board["row_#{comp_row}".to_sym][comp_col] = "O"
+    comp_col = @new_board.playable_col.sample
+    comp_row = @new_board.open_row[comp_col]
+    @new_board.board["row_#{comp_row}".to_sym][comp_col] = "O"
 
-      @new_board.open_row[comp_col] -= 1
-      if @new_board.open_row[comp_col] == 0
-        @new_board.playable_col.delete(comp_col)
-      end
+    @new_board.open_row[comp_col] -= 1
+    if @new_board.open_row[comp_col] == 0
+      @new_board.playable_col.delete(comp_col)
+    end
+    if @new_board.playable_col == []
+      # Check win condition
+      puts "Tie game!"
     end
   end
   
