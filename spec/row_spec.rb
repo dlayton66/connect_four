dcc148e0d3becb89268955126090779061c5d06a
Alex Pitzel <pitzelalex@gmail.com>
2022-10-25 13:45:17 -0400

    test: Update 'has tring of dynamic length' to reflect multiple possible lengths

--- spec/row_spec.rb
+++ spec/row_spec.rb
@@ -10,9 +10,10 @@ describe Row do
     end
     
     it 'has string of dots of dynamic length' do
-      new_row = Row.new(7)
-
-      expect(new_row.row).to eq(".......")
+      new_row_1 = Row.new(7)
+      new_row_2 = Row.new(10)
+      expect(new_row_1.row).to eq(".......")
+      expect(new_row_2.row).to eq("..........")
     end
   end
 end
