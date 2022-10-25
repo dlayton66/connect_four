25270f267f070058baca161e551be66dbe6b2522
Alex Pitzel <pitzelalex@gmail.com>
2022-10-25 13:25:08 -0400

    test: Add Row has string of dynamic length

--- spec/row_spec.rb
+++ spec/row_spec.rb
@@ -4,9 +4,15 @@ require './lib/row'
 describe Row do
   describe '#initialize' do
     it 'is an instance of Row' do
-      new_row = Row.new
+      new_row = Row.new(7)
 
       expect(new_row).to be_a Row 
+    end
+    
+    it 'has string of dots of dynamic length' do
+      new_row = Row.new(7)
+
+      expect(new_row.row).to eq(".......")
     end
   end
 end
