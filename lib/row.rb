d7c157245e103230af2abc4cd5166fa236021eee
Alex Pitzel <pitzelalex@gmail.com>
2022-10-25 13:44:33 -0400

    feat: Make Row pass 'has string of dynamic length' test

--- lib/row.rb
+++ lib/row.rb
@@ -1,5 +1,13 @@
 class Row
-  def initialize
+  attr_reader :row
 
+  def initialize(num)
+    @row = "." * num
   end
+
+  def set_row_0(num)
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    @row = alphabet[0,num]
+  end
+  # update_row places piece in correct index position
 end
